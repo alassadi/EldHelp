74a0c2151c4cda50173d69f2a31297922cb33088
fatsay <fatih_sayilir@hotmail.com>
2018-05-02 16:13:24 +0200

    Medicine List,Medicine Activity

--- app/src/main/java/com/company/eldhelp/MedicineActivity.java
+++ app/src/main/java/com/company/eldhelp/MedicineActivity.java
@@ -2,12 +2,55 @@ package com.company.eldhelp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Toast;
+
+import com.company.eldhelp.Interface.MedicineOnClickListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MedicineActivity extends AppCompatActivity {
+
+
+    public RecyclerView recyclerView;
+    public RecyclerView.Adapter adapter;
+    public RecyclerView.LayoutManager layoutManager;
+
+    private List<Medicine>medicines=new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_medicine);
+
+        //Database connection
+
+        Medicine fatih=new Medicine("alvedon","20:30");
+        medicines.add(fatih);
+        Medicine abood=new Medicine("Panadol","08:00");
+        medicines.add(abood);
+
+
+
+        //recyclerView
+
+        recyclerView=findViewById(R.id.medicine_recycler_view);
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager=new LinearLayoutManager(MedicineActivity.this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        adapter=new MedicineViewAdapter(this, medicines, new MedicineOnClickListener() {
+            @Override
+            public void onClick(View v, int position) {
+
+                //on click lister for recylerView
+                Toast.makeText(getApplicationContext(),"Test Onclick",Toast.LENGTH_LONG).show();
+            }
+        });
+        recyclerView.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/company/eldhelp/MedicineViewAdapter.java
+++ app/src/main/java/com/company/eldhelp/MedicineViewAdapter.java
@@ -1,11 +1,78 @@
 package com.company.eldhelp;
 
 import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.company.eldhelp.Interface.MedicineOnClickListener;
+
+import java.util.List;
 
 /**
  * Created by fatih on 2018-05-02.
  */
 
-public class MedicineViewAdapter{
+public class MedicineViewAdapter extends RecyclerView.Adapter<MedicineViewAdapter.Viewholder>{
+
+    Context context;
+    List<Medicine>medicines;
+    MedicineOnClickListener listener;
+
+    public MedicineViewAdapter(Context context, List<Medicine> medicines, MedicineOnClickListener listener) {
+        this.context = context;
+        this.medicines = medicines;
+        this.listener = listener;
+    }
+
+    public class Viewholder extends RecyclerView.ViewHolder{
+
+        public TextView medicineName, medicineTime;
+        public RelativeLayout medicine_row;
+
+        public Viewholder(final View itemView) {
+            super(itemView);
+
+            medicineName=itemView.findViewById(R.id.textView_medicine_name);
+            medicineTime=itemView.findViewById(R.id.textView_medicine_time);
+            medicine_row=itemView.findViewById(R.id.medicine_row);
+
+        }
+    }
+
+
+    @Override
+    public Viewholder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.medicine_row_list,parent,false);
+        final  Viewholder viewholder=new Viewholder(view);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listener.onClick(v,viewholder.getLayoutPosition());
+
+            }
+        });
+        return viewholder;
+    }
+
+    @Override
+    public void onBindViewHolder(Viewholder holder, int position) {
+
+        Medicine medicine=medicines.get(position);
+        holder.medicineName.setText(medicine.getName());
+        holder.medicineTime.setText(medicine.getTime());
+        holder.medicine_row.setTag(holder);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return medicines.size();
+    }
+
 
 }
