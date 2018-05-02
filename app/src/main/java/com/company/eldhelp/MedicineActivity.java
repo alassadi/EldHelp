68d0f9166cf9a81ab49a7f690c5b97bf6f1c8dbb
fatsay <fatih_sayilir@hotmail.com>
2018-05-02 14:18:52 +0200

    interface

--- app/src/main/java/com/company/eldhelp/MedicineActivity.java
+++ app/src/main/java/com/company/eldhelp/MedicineActivity.java
@@ -3,14 +3,11 @@ package com.company.eldhelp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MedicineActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-
+        setContentView(R.layout.activity_medicine);
     }
 }
---
+++ app/src/main/java/com/company/eldhelp/MedicineViewAdapter.java
@@ -0,0 +1,11 @@
+package com.company.eldhelp;
+
+import android.content.Context;
+
+/**
+ * Created by fatih on 2018-05-02.
+ */
+
+public class MedicineViewAdapter{
+
+}
