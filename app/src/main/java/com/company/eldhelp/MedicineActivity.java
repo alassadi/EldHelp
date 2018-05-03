2d36760bf9c4ffe45eff05d2be45f5908746cb69
Abdulrahman Alassadi <alassadi@live.com>
2018-05-03 16:09:23 +0200

    add medicine function

--- app/src/main/java/com/company/eldhelp/MedicineActivity.java
+++ app/src/main/java/com/company/eldhelp/MedicineActivity.java
@@ -1,20 +1,21 @@
 package com.company.eldhelp;
 
-import android.os.Handler;
+import android.app.TimePickerDialog;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
+import android.widget.TimePicker;
 import android.widget.Toast;
 import com.company.eldhelp.Interface.MedicineOnClickListener;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
-
 import android.content.DialogInterface;
 
 public class MedicineActivity extends AppCompatActivity {
@@ -34,18 +35,13 @@ public class MedicineActivity extends AppCompatActivity {
         setContentView(R.layout.activity_medicine);
 
         //Database connection
-
-
         addButton = findViewById(R.id.button_addMedicine);
         sqliteHelper = new Database(this);
-
         ArrayList<Medicine> medicines = sqliteHelper.getAllElements();
 
         //recyclerView
-
         recyclerView = findViewById(R.id.medicine_recycler_view);
         recyclerView.setHasFixedSize(true);
-
         layoutManager = new LinearLayoutManager(MedicineActivity.this);
         recyclerView.setLayoutManager(layoutManager);
 
@@ -67,39 +63,49 @@ public class MedicineActivity extends AppCompatActivity {
         });
     }
 
-
     private void showInputDialog() {
+        LayoutInflater inflater = getLayoutInflater();
+        View alertLayout = inflater.inflate(R.layout.layout_custom_dialog, null);
+        final EditText medicineName = alertLayout.findViewById(R.id.et_username);
+        final EditText medicineTime = alertLayout.findViewById(R.id.et_email);
+
         final AlertDialog.Builder alertDialog = new AlertDialog.Builder(MedicineActivity.this);
-        alertDialog.setTitle("Add Medicine");
-        alertDialog.setMessage("Enter Your Medicine: ");
+        alertDialog.setCancelable(false);
+        alertDialog.setView(alertLayout);
 
-        LinearLayout linearLayout = new LinearLayout(MedicineActivity.this);
-        linearLayout.setOrientation(LinearLayout.VERTICAL);
-        final EditText medicineName = new EditText(MedicineActivity.this);
-        medicineName.setHint("Name");
-        linearLayout.addView(medicineName);
+        medicineTime.setOnClickListener(new View.OnClickListener() {
 
-        final EditText medicineTime = new EditText(MedicineActivity.this);
-        medicineTime.setHint("Time");
-        linearLayout.addView(medicineTime);
+            @Override
+            public void onClick(View v) {
+                Calendar mcurrentTime = Calendar.getInstance();
+                int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
+                int minute = mcurrentTime.get(Calendar.MINUTE);
 
-
-        alertDialog.setView(linearLayout);
-        //alerDialog.setIcon(R.drawable.ic_shopping_cart_black_24dp);
-
-
+                TimePickerDialog mTimePicker;
+                mTimePicker = new TimePickerDialog(MedicineActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                        medicineTime.setText(selectedHour + ":" + selectedMinute);
+                    }
+                }, hour, minute, true);
+                mTimePicker.setTitle("Select Time");
+                mTimePicker.show();
+            }
+        });
         alertDialog.setPositiveButton("Add", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
+                String name = medicineName.getText().toString();
+                String time = medicineTime.getText().toString();
 
-
-                Medicine medicine = new Medicine(String.valueOf(medicineName.getText()), String.valueOf(medicineTime.getText()));
-                sqliteHelper.addMedicine(medicine);
-
-                Toast.makeText(getApplicationContext(), "Your Medicine is added!", Toast.LENGTH_LONG).show();
-
-
-                finish();
+                if (medicineName.getText().length() > 0 && medicineTime.getText().length() > 0) {
+                    Medicine medicine = new Medicine(name, time);
+                    sqliteHelper.addMedicine(medicine);
+                    Toast.makeText(getApplicationContext(), "Your Medicine is added!", Toast.LENGTH_LONG).show();
+                    dialogInterface.dismiss();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Your Medicine is Not added!", Toast.LENGTH_LONG).show();
+                }
             }
         });
         alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -109,7 +115,7 @@ public class MedicineActivity extends AppCompatActivity {
             }
         });
 
-        alertDialog.show();
-
+        AlertDialog dialog = alertDialog.create();
+        dialog.show();
     }
 }
