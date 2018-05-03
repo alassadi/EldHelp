65b5242c5a3ee53c8245aeeaba230aabf59b615b
Paulina0710 <paulina.wittich@gmx.de>
2018-05-03 16:59:14 +0200

    layout main activity

--- app/src/main/java/com/company/eldhelp/MainActivity.java
+++ app/src/main/java/com/company/eldhelp/MainActivity.java
@@ -2,6 +2,8 @@ package com.company.eldhelp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity{
 
