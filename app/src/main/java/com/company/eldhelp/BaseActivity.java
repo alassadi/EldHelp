7bb89d952f80d9ba678f9ba040ca3435d7d9fd21
Hankern1996 <hannah_kern2007@web.de>
2018-05-04 12:40:12 +0200

    Merge branch 'master' of https://github.com/alassadi/EldHelp into Hannah
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser
    
    Menue for the main page

--- app/src/main/java/com/company/eldhelp/BaseActivity.java
+++ app/src/main/java/com/company/eldhelp/BaseActivity.java
@@ -1,19 +1,39 @@
 package com.company.eldhelp;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
-public abstract class BaseActivity extends AppCompatActivity{
+public abstract class BaseActivity extends AppCompatActivity implements  NavigationView.OnNavigationItemSelectedListener {
+    private static final String EXTRA_OPEN_NAVIGATION = "com.company.eldhelp";
     android.support.v7.widget.Toolbar toolbar;
     int drawerLayout = 0;
     int navView = 0;
     int mToolbar= 0;
+    private String mString;
+
 
     @Override
     protected void onCreate (Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(getLayoutResource());
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mString = getIntent().getStringExtra(EXTRA_OPEN_NAVIGATION);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
     }
     protected abstract int getLayoutResource();
