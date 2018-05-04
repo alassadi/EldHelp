f696ad3db6e5b0341f51ad5bff84db55c1351968
Hankern1996 <hannah_kern2007@web.de>
2018-05-04 12:26:12 +0200

    Merge branch 'master' of https://github.com/alassadi/EldHelp into Hannah
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser
    
    Menue for the main page

---
+++ app/src/main/java/com/company/eldhelp/BaseActivity.java
@@ -0,0 +1,23 @@
+package com.company.eldhelp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public abstract class BaseActivity extends AppCompatActivity{
+    android.support.v7.widget.Toolbar toolbar;
+    int drawerLayout = 0;
+    int navView = 0;
+    int mToolbar= 0;
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutResource());
+
+
+    }
+    protected abstract int getLayoutResource();
+
+
+
+}
